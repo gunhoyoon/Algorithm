d625f8a61a76980784fca55029aaf617816a5544
gunhoyoon <rkdus5964@gmail.com>
2022-10-05 23:49:57 +0900

    해시 + 맵 사용,맵  메소드 사용

--- algorithm/solve.js
+++ algorithm/solve.js
@@ -1099,3 +1099,54 @@
 
 // let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 // console.log(solution(3, a));
+
+//
+//
+// 학급 회장 / Hash Map
+// // 흔히 사용했던 자료 구조가 아닌 Map을 사용한것임
+// function solution(s) {
+//   let answer = "";
+//   let sh = new Map(); // new Map() = 맵 생성
+//   for (let x of s) {
+//     if (sh.has(x))
+//       sh.set(
+//         x, // 키
+//         sh.get(x) + 1 // 값
+//       );
+//     // true 시 get을 사용해 가져온 x의 값 + 1 해서 저장해줄거임
+//     // has(x) - x의 요소안에 key가 있는지 ? 불리언으로 반환
+//     // set(key, value) key = k , value = get(x) + 1 // key를 이용해 value를 저장한다.
+//     // get = key 에 해당하는 값을 반환한다. 없을 시 undefined 반환
+//     else sh.set(x, 1); // false 시 키 x에 1의 값을 넣어줄거다
+//   }
+//   // sh 내부
+
+//   // key = "B" 등 , val = 3 등 이런식으로 할당되는거임
+//   let max = Number.MIN_SAFE_INTEGER;
+//   for (let [key, val] of sh) {
+//     // sh 자체가 key와 value를 가지고 있기 때문에 순회 가능
+//     // 0 {"B" => 3}
+//     // 1 {"A" => 3}
+//     // 2 {"C" => 5}
+//     // 3 {"D" => 2}
+//     // 4 {"E" => 2}
+//     if (val > max) {
+//       max = val; // max , val 투표 숫자임.
+//       answer = key; // 그 투표를 받은 C 라는 key가 answer로 들어가야함
+//     }
+//   }
+//   return answer;
+// }
+// let s = "BACBACCACCBDEDE";
+// console.log(solution(s));
+
+// // Map , Set 찾아놓은거 여러번 보고 익히기
+
+// // new Map() – 맵을 만듭니다.
+// // map.set(key, value) – key를 이용해 value를 저장합니다.
+// // map.get(key) – key에 해당하는 값을 반환합니다. key가 존재하지 않으면 undefined를 반환합니다.
+// // map.has(key) – key가 존재하면 true, 존재하지 않으면 false를 반환합니다.
+// // map.delete(key) – key에 해당하는 값을 삭제합니다.
+// // map.clear() – 맵 안의 모든 요소를 제거합니다.
+// // map.size – 요소의 개수를 반환합니다.
+// // 맵 + 해시 알고리즘 자체가 해시맵이다
--- algorithm/test.js
+++ algorithm/test.js
@@ -1,38 +1,7 @@
-// 학급 회장 / Hash Map
-function solution(s) {
-  let answer = "";
-  let sh = new Map();
-  for (let x of s) {
-    if (sh.has(x)) sh.set(x, sh.get(x) + 1);
-    // has(x) - x의 요소안에 key가 있는지 ? 불리언으로 반환
-    else sh.set(x, 1);
-  }
-  // sh 내부
-  // 0 {"B" => 3}
-  // 1 {"A" => 3}
-  // 2 {"C" => 5}
-  // 3 {"D" => 2}
-  // 4 {"E" => 2}
-  // key = "B" 등 , val = 3 등 이런식으로 할당되는거임
-  let max = Number.MIN_SAFE_INTEGER;
-  for (let [key, val] of sh) {
-    // console.log(key, val);
-    if (val > max) {
-      max = val; // max , val 투표 숫자임.
-      answer = key; // 그 투표를 받은 C 라는 key가 answer로 들어가야함
-    }
-  }
+function solution() {
+  let answer;
+
   return answer;
 }
-let s = "BACBACCACCBDEDE";
-console.log(solution(s));
 
-// Map , Set 찾아놓은거 여러번 보고 익히기
-
-// new Map() – 맵을 만듭니다.
-// map.set(key, value) – key를 이용해 value를 저장합니다.
-// map.get(key) – key에 해당하는 값을 반환합니다. key가 존재하지 않으면 undefined를 반환합니다.
-// map.has(key) – key가 존재하면 true, 존재하지 않으면 false를 반환합니다.
-// map.delete(key) – key에 해당하는 값을 삭제합니다.
-// map.clear() – 맵 안의 모든 요소를 제거합니다.
-// map.size – 요소의 개수를 반환합니다.
+console.log(solution());
