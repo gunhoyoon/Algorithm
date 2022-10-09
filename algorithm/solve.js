238c0cdb8cc9220840e71a29845420d9db9e6a53
gunhoyoon <rkdus5964@gmail.com>
2022-10-09 18:00:01 +0900

    모든 알고리즘 탐색 종료

--- algorithm/solve.js
+++ algorithm/solve.js
@@ -1179,3 +1179,48 @@
 // map 메소드 잘 사용하고 값 저장해서 불러오기 , 비교하기
 
 // // 어떻게 매칭이 되나 싶었지만 기존에 sh에 들어가게 위에서 설정해놨기 떄문에 매치가 되는거였음
+
+//
+//
+//// 모든 아나그램 찾기 / 슬라이딩 윈도우 , 해시 맵 , 투 포인트 알고리즘
+// function compareMaps(map1, map2) {
+//   if (map1.size !== map2.size) return false;
+//   for (let [key, val] of map1) {
+//     if (!map2.has(key) || map2.get(key) !== val) return false;
+//   }
+//   return true;
+// }
+
+// function solution(s, t) {
+//   let answer = 0;
+//   let th = new Map();
+//   let sh = new Map();
+
+//   for (let x of t) {
+//     if (th.has(x)) th.set(x, th.get(x) + 1);
+//     else th.set(x, 1);
+//   }
+
+//   let len = t.length - 1;
+
+//   for (let i = 0; i < len; i++) {
+//     if (sh.has(s[i])) sh.set(s[i], sh.get(s[i]) + 1);
+//     else sh.set(s[i], 1);
+//   }
+
+//   let lt = 0;
+
+//   for (let rt = len; rt < s.length; rt++) {
+//     if (sh.has(s[rt])) sh.set(s[rt], sh.get(s[rt]) + 1);
+//     else sh.set(s[rt], 1);
+//     if (compareMaps(sh, th)) answer++;
+//     sh.set(s[lt], sh.get(s[lt]) - 1);
+//     if (sh.get(s[lt]) === 0) sh.delete(s[lt]);
+//     lt++;
+//   }
+
+//   return answer;
+// }
+// let a = "bacaAacba";
+// let b = "abc";
+// console.log(solution(a, b));
