f9c4d769f673b1d3034345d9bba7d985f2dda92d
gunhoyoon <rkdus5964@gmail.com>
2022-10-18 16:29:24 +0900

    기존 배열 정렬 배열, 정렬

--- algorithm/solve.js
+++ algorithm/solve.js
@@ -1535,3 +1535,113 @@
 // let a = [11, 7, 5, 6, 10, 9];
 
 // console.log(solution(a));
+
+//
+//
+// // 삽입 정렬 식 LRU 구현(캐시)
+// function solution(size, arr) {
+//   let answer = Array.from({ length: size }, () => 0); // 캐시의 형태를 가짐
+
+//   arr.forEach((x) => {
+//     let pos = -1;
+//     // size : 5
+//     // i : 0 1 2 3 4
+//     // x = 1, 2, 3, 2, 6, 2, 3, 5, 7
+//     for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+//     // answer는 처음 다 0으로 세팅
+//     // 그래서 중복되는 값을 x가 만났을 때의 경우를 히트 라고 하는데
+//     // 그럴땐 가장 최근에 불린 데이터이기 때문에 이전 인덱스 값을 다음에 복사해두고
+//     // [0] 번째 자리에 중복된 인덱스 값을 넣어준다.
+//     // 일단 중복이 됐을 경우 i의 값을 Pos에 담아두기 때문에
+
+//     // 중복되지 않고 처음 들어가는 값일 경우
+//     // 제일 뒤에서 부터 현재 작업[0]까지 하나씩 땡겨준다.
+//     // 그래서 처음 캐시 내용은 1 0 0 0 0 상태가 된다.
+//     if (pos === -1) {
+//       for (let i = size - 1; i >= 1; i--) {
+//         answer[i] = answer[i - 1];
+//       }
+//     }
+//     // x가 돌다가 같은 값을 만난 경우
+//     // 이때는 그 자리부터 하나씩 앞으로 돌면서 기존 i에 i-1의 값을 넣어준다.
+//     // 그러고 다시 0번째에 현재 작업 x 할당
+//     else {
+//       for (let i = pos; i >= 1; i--) {
+//         answer[i] = answer[i - 1];
+//       }
+//     }
+//     answer[0] = x;
+//     // x의 값을 answer 0 번째에 넣어준다. 이때부터 arr 값이 들어가는거임
+//     // 현재 작업하는 1번이 최우선 순위에 들어가야하니 처음으로 넣어주는거임 [0] 번째 인덱스
+//     // 작업하는 값이 중복될 경우 그 i까지만 돌고 다시 처음 0번째 인덱스에 넣어준다. 최신 작업이니까.
+//     // 중복되는 값이 없을 경우 size -1 즉 4번째 인덱스(맨끝) 부터 돌면서 하나씩 옆으로 복사
+//     // (마지막 지워주기 새로운 값 추가하기)
+//   });
+
+//   return answer;
+// }
+// let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+// console.log(solution(5, arr));
+
+//
+//
+// answer.pop(); 배열의 마지막 요소 삭제
+// // answer.unshift(arr[1]); 배열 첫번째에 요소 추가해서 배열의 새 길이를 반환한다.
+
+// function solutin(size, arr) {
+//   let answer = Array.from({ length: size }, () => 0);
+//   arr.forEach((x) => {
+//     let pos = -1;
+//     for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+
+//     if (pos === -1) {
+//       answer.unshift(x);
+//       if (answer.length > size) answer.pop();
+//     } else {
+//       answer.splice(pos, 1);
+//       answer.unshift(x);
+//     }
+//   });
+// }
+
+// let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+// console.log(solution(5, arr));
+
+//
+//
+////  장난꾸러기 현수 / 정렬
+
+// function solution(arr) {
+//   let answer = [];
+//   let sortArr = arr.slice().sort((a, b) => a - b);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== sortArr[i]) answer.push(i + 1);
+//   }
+
+//   return answer;
+// }
+
+// let arr = [120, 130, 150, 150, 130, 150];
+// console.log(solution(arr));
+
+//
+//좌표 정렬(sort)
+// function solution(arr) {
+//   let answer = arr;
+//   arr.sort((a, b) => {
+//     if (a[0] === b[0]) return a[1] - a[1]; // y축으로 정렬
+//     else return a[0] - a[0]; // x 축으로 정렬
+//   });
+
+//   return answer;
+// }
+
+// let arr = [
+//   [1, 2],
+//   [1, 3],
+//   [2, 5],
+//   [2, 7],
+//   [3, 6],
+// ];
+
+// console.log(solution(arr));
--- algorithm/test.js
+++ algorithm/test.js
@@ -1,81 +1,20 @@
-// // 삽입 정렬 식 LRU 구현(캐시)
-function solution(size, arr) {
-  let answer = Array.from({ length: size }, () => 0);
-
-  arr.forEach((x) => {
-    let pos = -1;
-    // size : 5
-    // i : 0 1 2 3 4
-    // x = 1, 2, 3, 2, 6, 2, 3, 5, 7
-    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
-    // answer는 처음 다 0으로 세팅
-
-    // 그래서 위의 조건이 false일 경우 pos는 그대로 -1
-    // size - 1 = 4
-    //
-    if (pos === -1) {
-      for (let i = size - 1; i >= 1; i--) {
-        answer[i] = answer[i - 1];
-      }
-    } // if(x === answer[i]) 의 조건이 true 여서 pos에 i 값이 들어간 경우
-    else {
-      for (let i = pos; i >= 1; i--) {
-        answer[i] = answer[i - 1];
-      }
-    }
-    answer[0] = x;
-    // x의 값을 answer 0 번째에 넣어준다. 이때부터 arr 값이 들어가는거임
-    // 현재 작업하는 1번이 최우선 순위에 들어가야하니 처음으로 넣어주는거임 [0] 번째 인덱스
-    // 작업하는 값이 중복될 경우 그 i까지만 돌고 다시 처음 0번째 인덱스에 넣어준다. 최신 작업이니까.
-    // 중복되는 값이 없을 경우 size -1 즉 4번째 인덱스(맨끝) 부터 돌면서 하나씩 옆으로 복사
-    // (마지막 지워주기 새로운 값 추가하기)
+//좌표 정렬(sort)
+function solution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - a[1]; // y축으로 정렬
+    else return a[0] - a[0]; // x 축으로 정렬
   });
 
   return answer;
 }
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-console.log(solution(5, arr));
 
-// answer.pop(); 배열의 마지막 요소 삭제
-// answer.unshift(arr[1]); 배열 첫번째에 요소 추가해서 배열의 새 길이를 반환한다.
+let arr = [
+  [1, 2],
+  [1, 3],
+  [2, 5],
+  [2, 7],
+  [3, 6],
+];
 
-// function solution(size, arr) {
-//   let answer = [];
-//   arr.forEach((x) => {
-//     let pos = -1;
-//     for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
-//     if (pos === -1) {
-//       answer.unshift(x);
-//       if (answer.length > size) answer.pop();
-//     } else {
-//       answer.splice(pos, 1);
-//       answer.unshift(x);
-//     }
-//   });
-//   return answer;
-// }
-
-// let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-// console.log(solution(5, arr));
-
-function solution(size, arr) {
-  let answer = Array.from({ length: size }, () => 0);
-  arr.forEach((x) => {
-    let pos = -1;
-    for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
-    if (pos === -1) {
-      for (let i = size - 1; i >= 1; i--) {
-        answer[i] = answer[i - 1];
-      }
-    } else {
-      for (let i = pos; i >= 1; i--) {
-        answer[i] = answer[i - 1];
-      }
-    }
-    answer[0] = x;
-  });
-  return answer;
-}
-
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-console.log(solution(5, arr));
+console.log(solution(arr));
