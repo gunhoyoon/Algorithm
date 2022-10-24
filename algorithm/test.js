9d47b8c264533893914c078f41a507e63b38a9f2
gunhoyoon <rkdus5964@gmail.com>
2022-10-24 21:39:57 +0900

    아스키 코드

--- algorithm/test.js
+++ algorithm/test.js
@@ -9,8 +9,10 @@
 //   }
 //   T_line.sort((a, b) => {
 //     if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
-//     // 아스키코드 상 s가 e보다 큼
+//     // 시간이 같다면 s = 들어옴 , e = 나감 인데 같은 수의 e 인게 먼저 나가야하기때문에
+//     // e = 나감 이기때문에 e가 먼저기때문에 숫자가 같을 시 아스키코드로 오름차순 해줌
 //     else return a[0] - b[0];
+//     //  같지않다면 시간으로 오름차순 정렬
 //   });
 //   let cnt = 0;
 //   for (let x of T_line) {
@@ -32,3 +34,32 @@
 //   [5, 14],
 // ];
 // console.log(solution(arr));
+
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let T_line = [];
+  for (let x of times) {
+    T_line.push([x[0], "s"]);
+    T_line.push([x[1], "e"]);
+  }
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === "s") cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
+
+let arr = [
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
+];
+console.log(solution(arr));
